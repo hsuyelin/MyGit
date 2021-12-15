832d97c4c738055ecf98a57319115a27b37a5e97
xuyanling <xuyanling@huami.com>
2021-12-15 10:47:04 +0800

    build |> 初始化工程项目

---
+++ gitCommand/gitCommand/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Created on 2021/12/15
+//  Description <#文件描述#>
+//  PD <#产品文档地址#>
+//  Design <#设计文档地址#>
+//  Copyright © 2021 Zepp Health. All rights reserved.
+//  @author <#zepp.health#>(<#zepp.health#>@zepp.com)   
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
