056c231da86e321ae5dbbb2142875ce1bbd72416
xuyanling <xuyanling@huami.com>
2022-05-27 11:07:06 +0800

    fix |> [MyGit]修复背景色为白色的问题

--- gitCommand/gitCommand/ViewController.swift
+++ gitCommand/gitCommand/ViewController.swift
@@ -16,7 +16,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        view.backgroundColor = .white
+        view.backgroundColor = .cyan
         test()
     }
     
